d65efe97a7582f4c9e2db227433f9a07546846fe
邓卫星g <455613844@qq.com>
2018-04-08 17:32:57 +0800

    B

---
+++ TestA/ViewController.m
@@ -0,0 +1,34 @@
+//
+//  ViewController.m
+//  TestA
+//
+//  Created by dengwx on 2018/4/8.
+//  Copyright © 2018年 Ehi. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)logA {
+    
+    NSLog(@"A 说的");
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
